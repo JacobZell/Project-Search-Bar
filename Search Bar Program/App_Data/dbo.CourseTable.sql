cdae4c6ba253f2c3fed3e2057a6c0a18d192eceb
Mr. Polar Bear <78175660+delahautl@users.noreply.github.com>
2021-05-20 15:34:16 -0500

    Exam Submission

---
+++ Search Bar Program/App_Data/dbo.CourseTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CourseTable] (
+    [Course_ID]           INT  NOT NULL,
+    [Course_Name]         TEXT NULL,
+    [Course_Descriuption] TEXT NULL,
+    [Professor_Name]      TEXT NULL,
+    [Lecture_Hours]       INT  NULL,
+    [Registered_Students] INT  NULL,
+    [Review_of_Course]    INT  NULL,
+    [Book_Link]           TEXT NULL,
+    PRIMARY KEY CLUSTERED ([Course_ID] ASC)
+);
---
+++ Search Bar Program/App_Data/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Course ID]           INT  NOT NULL,
+    [Course Name]         TEXT NULL,
+    [Course Descriuption] TEXT NULL,
+    [Professor Name]      TEXT NULL,
+    [Lecture Hours]       INT  NULL,
+    [Registered Students] INT  NULL,
+    [Review of Course]    INT  NULL,
+    [Book Link]           TEXT NULL,
+    PRIMARY KEY CLUSTERED ([Course ID] ASC)
+);
